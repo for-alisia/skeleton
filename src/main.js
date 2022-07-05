fe9957b6d5bfa53ad2abfcd861b8144756bac40b
Elena <for-alisia@yandex.ru>
2022-07-05 09:35:54 +0200

    Prepared main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
 class App {
-  run() {
+  counter = null;
+
+  create() {
+    this.counter = 0;
     const name = 'World';
     console.log(`Hello ${name}`);
   }
+
+  update = async () => {
+    if (!counter) return false;
+
+    this.counter++;
+    console.log([1, 2, 3, [4, 5]].flat());
+
+    return true;
+  };
 }
 
 const app = new App();
-app.run();
+app.create();
+app.update().then((res) => console.log(res));
