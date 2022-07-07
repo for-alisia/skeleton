9e02108bf574db1230e4dc7f2c6f4a3fef15c4e5
Elena <for-alisia@yandex.ru>
2022-07-07 10:58:38 +0200

    Changed app data

--- src/main.js
+++ src/main.js
@@ -1,22 +1,48 @@
-class App {
-  counter = null;
+// We want to finish with something like this:
+// import Log from './log';
+// import Calc from './calc';
+// import img from './assets/webpack.png';
+// import './css/main.css';
 
-  create() {
-    this.counter = 0;
-    const name = 'World';
-    console.log(`Hello ${name}`);
+// We want to have this class as separate module
+class Calc {
+  add(...args) {
+    return args.reduce((sum, elem) => sum + elem, 0);
   }
 
-  update = async () => {
-    if (!counter) return false;
+  partial(x, y) {
+    return x * y;
+  }
 
-    this.counter++;
-    console.log([1, 2, 3, [4, 5]].flat());
-
-    return true;
+  test = (msg = 'Hello!') => {
+    console.log(msg);
   };
 }
 
-const app = new App();
-app.create();
-app.update().then((res) => console.log(res));
+// This class should be a separate module as well
+class Log {
+  log(msg) {
+    console.log('=========');
+    console.log(msg);
+    console.log('=========');
+  }
+}
+
+const log = new Log();
+const calc = new Calc();
+
+const result = calc.add(1, 2, 3);
+
+log.log(calc.add(1, 2, 3));
+
+const root = document.getElementById('root');
+root?.textContent = `The result is ${result}`;
+
+const image = document.createElement('img');
+image.src = img;
+document.body.appendChild(image);
+
+calc.test();
+const withSeven = calc.partial(?, 7);
+
+log.log(withSeven(2));
